33f7a1d35146e4bd5874d89ef9b1553eb0beaa1b
“sarthak <sarthakgangrade0@gmail.com>
2021-09-22 04:13:09 +0530

    day 7

---
+++ day 7/digitsReapetedTwice.sh
@@ -0,0 +1,12 @@
+index=0
+for(( i = 0; i <= 100;  ))
+  do
+    if (( i % 11 == 0 ))
+    then
+        array[index++]=$i
+        (( i += 11))
+    else
+        (( i++ ))
+    fi
+  done
+echo "array: ${array[@]} "
