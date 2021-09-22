259b6190f823ccae427d43e200f9ddcda3c0e192
“sarthak <sarthakgangrade0@gmail.com>
2021-09-22 21:04:36 +0530

    add numberinwordCase.sh

--- day 5/numberinwordCase.sh
+++ day 5/numberinwordCase.sh
@@ -1,20 +1,25 @@
-#!/bin/bash -x
-
+#!bin/bash -x
 read -p "Enter a week number : " n
 case $n in
-   "0")echo "SUNDAY"
+   "0")echo "ZERO"
    ;;
-   "1")echo "MONDAY"
+   "1")echo "ONE"
    ;;
-   "2")echo "TUESDAY"
+   "2")echo "TWO"
    ;;
-   "3")echo "WEDNESDAY"
+   "3")echo "THREE"
    ;;
-   "4")echo "THURSDAY"
+   "4")echo "FOUR"
    ;;
-   "5")echo "FRIDAY"
+   "5")echo "FIVE"
    ;;
-   "6")echo "SATURDAY"
+   "6")echo "SIX"
+   ;;
+   "7")echo "SEVEN"
+   ;;
+   "8")echo "EIGHT"
+   ;;
+   "9")echo "NINE"
    ;;
    *)echo "INVALID ENTRY"
    ;;
