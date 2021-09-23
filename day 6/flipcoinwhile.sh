2b9da8f3bca2d8814764e5293efaa554cdcc56d3
“sarthak <sarthakgangrade0@gmail.com>
2021-09-23 12:04:42 +0530

    add flipcoinwhile.sh

---
+++ day 6/flipcoinwhile.sh
@@ -0,0 +1,16 @@
+i=1
+randomNumber=$(( RANDOM%2 ))
+if (( $randomNumber == 0 ))
+then 
+	while (( $i<=11 ))
+	do
+		echo "Heads"
+		i=$i+1
+	done
+else
+	while (( $i<=11 ))
+	do
+		echo "Tails"
+		i=$i+1
+	done
+fi
